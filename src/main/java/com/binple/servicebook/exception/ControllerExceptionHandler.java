6dbb2f980555bb4bb0affc808c1803fa84e9a43a
Petar Cerovic <quadrition@gmail.com>
2020-11-25 23:39:13 +0100

    Added dependency validating and method argument handling

--- src/main/java/com/binple/servicebook/exception/ControllerExceptionHandler.java
+++ src/main/java/com/binple/servicebook/exception/ControllerExceptionHandler.java
@@ -1,8 +1,20 @@
 package com.binple.servicebook.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+      HttpStatus status, WebRequest request) {
+
+    return new ResponseEntity<>(ex.getBindingResult().getAllErrors(), HttpStatus.BAD_REQUEST);
+  }
 }
