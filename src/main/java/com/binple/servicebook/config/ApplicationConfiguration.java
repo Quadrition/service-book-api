f1d5192e91e2923415991b6dcb99093b463a947c
Petar Cerovic <quadrition@gmail.com>
2020-11-25 23:44:57 +0100

    Added model mapper and configuration

---
+++ src/main/java/com/binple/servicebook/config/ApplicationConfiguration.java
@@ -0,0 +1,16 @@
+package com.binple.servicebook.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.springframework.context.annotation.Bean;
+
+public class ApplicationConfiguration {
+
+  @Bean
+  public ModelMapper modelMapper() {
+
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(AccessLevel.PRIVATE);
+    return modelMapper;
+  }
+}
