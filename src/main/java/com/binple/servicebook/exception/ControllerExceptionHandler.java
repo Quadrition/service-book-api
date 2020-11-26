bfc89b57906d757df360b96bf21b5502aa7464de
Petar Cerovic <quadrition@gmail.com>
2020-11-26 18:53:44 +0100

    Added exception handling for providing invalid parameters in controller

--- src/main/java/com/binple/servicebook/exception/ControllerExceptionHandler.java
+++ src/main/java/com/binple/servicebook/exception/ControllerExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.binple.servicebook.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,16 +15,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(VehicleAlreadyExistsException.class)
-  protected ResponseEntity<String> handleVehicleAlreadyExistsException(VehicleAlreadyExistsException ex,
+  protected ResponseEntity<Object> handleVehicleAlreadyExistsException(VehicleAlreadyExistsException ex,
       WebRequest request) {
 
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
   @ExceptionHandler(VehicleNotFoundException.class)
-  protected ResponseEntity<String> handleVehicleNotFoundException(VehicleNotFoundException ex, WebRequest request) {
+  protected ResponseEntity<Object> handleVehicleNotFoundException(VehicleNotFoundException ex, WebRequest request) {
 
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex,
+      WebRequest request) {
+    // TODO not working
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @Override
@@ -30,5 +39,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
       HttpStatus status, WebRequest request) {
 
     return new ResponseEntity<>(ex.getBindingResult().getAllErrors(), HttpStatus.BAD_REQUEST);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 }
